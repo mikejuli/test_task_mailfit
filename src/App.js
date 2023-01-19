5b02da90b40d7661611b8ed8bc673f8634131326
Mikhail <juliinstant88@gmail.com>
2023-01-19 23:18:42 +0800

    created basic logic createNode_deleteNode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState, useEffect} from 'react';
 function App() {
+
+  //mock data
+
+  const [mockData,setData] = useState([]);
+
+
+  function createNewNode(){
+    setData([...mockData, {id:5,name:'anotherList5',todoList:'123'}])
+    console.log(mockData);
+
+    localStorage.setItem('list', JSON.stringify([...mockData, {id:5,name:'anotherList5',todoList:'123'}]))
+  }
+
+  function deleteNode(id){
+
+    let deletedId;
+
+    mockData.forEach((x,index)=>{ if(x.id===id) { deletedId = index } })
+
+    if(deletedId){
+      var s = mockData.splice(deletedId,1);
+     setData([...mockData])
+  }
+    console.log(s, mockData)
+  }
+
+
+
+  useEffect(()=>{
+
+    // localStorage.setItem('list', JSON.stringify([{id: 1, name: 'get to groceries', todoList: 'do smth'},{id: 2, name: 'go to the store', todoList: 'do smth'}]) )
+
+    const items = JSON.parse(localStorage.getItem('list'))
+
+    console.log(items)
+
+   if(items){setData(items)}
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    {console.log('was rendered')}
+  {mockData.map((x)=><div>{x.name}{x.todoList}</div>)}
+
+    <button id ='add' onClick = {createNewNode}>Create</button>
+    <button id ='add'onClick = {()=>{deleteNode(5)}}>Delete</button>
+
     </div>
   );
 }
